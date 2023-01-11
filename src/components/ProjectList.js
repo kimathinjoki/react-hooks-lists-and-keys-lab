beddfc25ad1192e597283b9d0d9ed7a277631b6d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-01-11 09:27:01 -0800

    add item list

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,12 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* < ProjectItem key={projects.id} name={projects.name} about={projects.about} technology={projects.technology}  /> */}
+        {projects.map(()=>{
+          <ProjectItem />
+        })}
+      </div>
     </div>
   );
 }
