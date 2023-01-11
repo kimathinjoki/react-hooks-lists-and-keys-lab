c5e3c1cc3490a431b2e7b7d8af29cc59c6b0fcba
kimathinjoki <dfkimathinjoki@gmail.com>
2023-01-11 10:05:35 -0800

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,8 +8,10 @@ function ProjectList({ projects }) {
       <h2>My Projects</h2>
       <div id="project-list">
         {/* < ProjectItem key={projects.id} name={projects.name} about={projects.about} technology={projects.technology}  /> */}
-        {projects.map(()=>{
-          <ProjectItem />
+        {projects.map((project)=>{
+          return (
+          <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>
+          )
         })}
       </div>
     </div>
